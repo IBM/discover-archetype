062016d8a2c6c302191d4c4b9c57ec268b4d3fe7
ton <ton@us.ibm.com>
2019-12-03 15:56:10 -0800

    Refactor the code for plotting the archetypes

--- python/show_archetype.py
+++ python/show_archetype.py
@@ -1,45 +1,67 @@
+##################
+### IMPORT MODULES
+##################
+
+### System
+import sys
+import os
+from fnmatch import fnmatch 
+
+### I/O
+import json
+import pickle
+
+### General Processing
+import numpy as np
+import scipy as sp
+import pandas as pd
+import networkx as nx
+import math
+import collections
+from collections import OrderedDict
+from collections import namedtuple
+DottedDict = namedtuple
+
+## DECOMPOSITION
+from sklearn.decomposition import NMF
+from scipy.linalg import svd
+from sklearn.model_selection import train_test_split
+
+### NLU
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1 as NLUV1
+from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOptions,ConceptsOptions,EntitiesOptions,KeywordsOptions,RelationsOptions,SyntaxOptions
 
 
-# From entities
-typ = 'entities'
-fig = plt.figure(figsize=(14, 12))
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
-n_archs   = 6
-threshold = 0.1
-for i in range(n_archs):
-    tot = wda.archetypes(typ = typ,n_archs=n_archs).f.T.apply(scale)[i].sum()
-    f =   wda.display_archetype(typ = typ,n_archs=n_archs, arch_nr=i, threshold = threshold, norm = scale)
-    ax = fig.add_subplot(2, 3, i+1)
-    ax.title.set_text('Key: Archetype '+str(i)+'\n'+str(int(100*f[i].sum()/tot))+'% of volume displayed')
-    sns.heatmap(f)
-fig.show()
+### Presentation / apps
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-# From concepts
-typ = 'concepts'
-fig = plt.figure(figsize=(14, 12))
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
-n_archs   = 6
-threshold = 0.1
-for i in range(n_archs):
-    tot = wda.archetypes(typ = typ,n_archs=n_archs).f.T.apply(scale)[i].sum()
-    f =   wda.display_archetype(typ = typ,n_archs=n_archs, arch_nr=i, threshold = threshold, norm = scale)
-    ax = fig.add_subplot(2, 3, i+1)
-    ax.title.set_text('Key: Archetype '+str(i+1)+'\n'+str(int(100*f[i].sum()/tot))+'% of volume displayed')
-    sns.heatmap(f)
-fig.show()
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import dash_table.FormatTemplate as FormatTemplate
+from dash_table.Format import Sign
+from dash.dependencies import Input, Output
 
-# From keywords
-typ = 'keywords'
-fig = plt.figure(figsize=(14, 12))
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
-n_archs   = 6
-threshold = 0.1
-for i in range(n_archs):
-    tot = wda.archetypes(typ = typ,n_archs=n_archs).f.T.apply(scale)[i].sum()
-    f =   wda.display_archetype(typ = typ,n_archs=n_archs, arch_nr=i, threshold = threshold, norm = scale)
-    ax = fig.add_subplot(2, 3, i+1)
-    ax.title.set_text('Key: Archetype '+str(i+1)+'\n'+str(int(100*f[i].sum()/tot))+'% of volume displayed')
-    sns.heatmap(f)
-fig.show()
+import plotly.graph_objs as go
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
 
-# From 
+def plot_archetypes(wda, feature_type):
+    # Create a plot for the number of archetypes desired 
+    fig = plt.figure(figsize=(14, 12))
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    n_archs   = 6
+    threshold = 0.1
+    for i in range(n_archs):
+        tot = wda.archetypes(typ=feature_type, n_archs=n_archs).f.T.apply(scale)[i].sum()
+        f =   wda.display_archetype(typ=feature_type, n_archs=n_archs, arch_nr=i, threshold=threshold, norm = scale)
+        ax = fig.add_subplot(2, 3, i+1)
+        ax.title.set_text('Key: Archetype '+str(i)+'\n'+str(int(100*f[i].sum()/tot))+'% of volume displayed')
+        sns.heatmap(f)
+    return fig
+
+
